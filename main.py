31ad4f8c04f7afe27f7b5f7287c770cbeedf9f77
kyomou <chonngai@gmail.com>
2026-03-13 13:18:58 +1100

    added api for skinbuilder and get weather

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
-from  WeatherClient import WeatherClient
+from  weather_client import WeatherClient
+from pydantic import BaseModel
+
 app = FastAPI()
 wc = WeatherClient()
+class skinData(BaseModel):
+    #integer for how skin react to sun exposure
+    skin_react: int
+    #string for area that spend most time in
+    area: str
+    #string for reason for going out    
+    reason: str
+    #string for experience with sunburns
+    sunburn_experience: str
+    #string for weekday time spend
+    weekday_time: str
+    #string for time of day spend outside
+    time_of_day: str
+    #string for how often applying sunscreen
+    sunscreen: str
+    #string for what doing in sun
+    do_in_sun: str
+class weatherData(BaseModel):
+    #string for city name
+    city: str
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -11,10 +33,17 @@ def read_root():
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
-@app.post("/weather/{city}")
-def get_weather(city: str):
+@app.post("/weather/")
+def get_weather(data: weatherData):
     try:
-        data = wc.get_weather(city)
-        return {"city": city, "weather": data}
+        response = wc.get_weather(data.city)
+        return {"city": data.city, "weather": response}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.post("/skinbuilder/")
+async def build_skin(data: skinData):
+    try:
+     return {"message": "Skin data processed successfully", "data": data}
     except Exception as e:
         return {"error": str(e)}
